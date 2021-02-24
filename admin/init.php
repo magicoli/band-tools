4409831b47a968e218886fadb00ce9edd89a7d6a
Olivier van Helden <olivier@van-helden.net>
2021-02-24 18:57:51 -0400

    we don't use settings yet, disabled for now

--- admin/init.php
+++ admin/init.php
@@ -8,8 +8,8 @@
 
 if ( ! defined( 'WPINC' ) ) die;
 
-require(plugin_dir_path(__FILE__) . 'menus.php');
-require(plugin_dir_path(__FILE__) . 'settings.php');
+// require(plugin_dir_path(__FILE__) . 'menus.php');
+// require(plugin_dir_path(__FILE__) . 'settings.php');
 // require(plugin_dir_path(__FILE__) . 'woocommerce.php');
 
 add_action( 'wp_loaded', 'disable_theme_registration_notice' );
