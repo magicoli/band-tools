f9c414964b86f55aff89783edfcd27c2bd31b0eb
Olivier van Helden <olivier@van-helden.net>
2021-02-24 14:06:12 -0400

    Initial commit

---
+++ band-tools.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Bands Tools
+ * Plugin URI:      https://git.magiiic.com/wordpress/band-tools
+ * Description:     Tools for bands, artists, musicians
+ * Author:          Olivier van Helden
+ * Author URI:      =https://magiiic.com/
+ * Text Domain:     band-tools
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Band_Tools
+ */
+
+// Your code starts here.
