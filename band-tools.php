e2a5cef50026e79de72f1f2ec888ae38acac4b47
Olivier van Helden <olivier@van-helden.net>
2021-02-24 14:11:10 -0400

    Added auto updater

--- band-tools.php
+++ band-tools.php
@@ -4,7 +4,7 @@
  * Plugin URI:      https://git.magiiic.com/wordpress/band-tools
  * Description:     Tools for bands, artists, musicians
  * Author:          Olivier van Helden
- * Author URI:      =https://magiiic.com/
+ * Author URI:      https://magiiic.com/
  * Text Domain:     band-tools
  * Domain Path:     /languages
  * Version:         0.1.0
@@ -12,4 +12,14 @@
  * @package         Band_Tools
  */
 
-// Your code starts here.
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) die;
+
+/** Enable plugin updates with license check **/
+require_once plugin_dir_path( __FILE__ ) . 'lib/wp-package-updater/class-wp-package-updater.php';
+$band_tools_updater = new WP_Package_Updater(
+	'https://magiiic.com',
+	wp_normalize_path( __FILE__ ),
+	wp_normalize_path( plugin_dir_path( __FILE__ ) ),
+	// true
+);
