fed3f8d23e0546d6433411a41885add7572fb543
Olivier van Helden <olivier@van-helden.net>
2021-02-27 21:18:00 -0400

    translations update

--- inc/custom-fields.php
+++ inc/custom-fields.php
@@ -6,11 +6,11 @@ if( function_exists('acf_add_local_field_group') ):
 
 acf_add_local_field_group(array(
 	'key' => 'group_6036ca9f301ee',
-	'title' => 'Album info',
+	'title' => __('Album info', 'band-tools'),
 	'fields' => array(
 		array(
 			'key' => 'field_60370454ae3d1',
-			'label' => 'Album band',
+			'label' => __('Album band', 'band-tools'),
 			'name' => 'album_band',
 			'type' => 'post_object',
 			'instructions' => '',
@@ -32,7 +32,7 @@ acf_add_local_field_group(array(
 		),
 		array(
 			'key' => 'field_6036cdea22a7b',
-			'label' => 'Release date',
+			'label' => __('Release date', 'band-tools'),
 			'name' => 'release_date',
 			'type' => 'date_picker',
 			'instructions' => '',
@@ -49,7 +49,7 @@ acf_add_local_field_group(array(
 		),
 		array(
 			'key' => 'field_6036caa495dc2',
-			'label' => 'Tracks',
+			'label' => __('Tracks', 'band-tools'),
 			'name' => 'tracks',
 			'type' => 'relationship',
 			'instructions' => '',
@@ -94,11 +94,11 @@ acf_add_local_field_group(array(
 
 acf_add_local_field_group(array(
 	'key' => 'group_6036da015238e',
-	'title' => 'Band info',
+	'title' => __('Band info', 'band-tools'),
 	'fields' => array(
 		array(
 			'key' => 'field_6037040409e52',
-			'label' => 'Album Band',
+			'label' => __('Album Band', 'band-tools'),
 			'name' => 'album_band',
 			'type' => 'relationship',
 			'instructions' => '',
@@ -125,7 +125,7 @@ acf_add_local_field_group(array(
 		),
 		array(
 			'key' => 'field_6036da45bee9d',
-			'label' => 'Songs',
+			'label' => __('Songs', 'band-tools'),
 			'name' => 'band',
 			'type' => 'relationship',
 			'instructions' => '',
@@ -170,11 +170,11 @@ acf_add_local_field_group(array(
 
 acf_add_local_field_group(array(
 	'key' => 'group_6036c84abbd81',
-	'title' => 'Song info',
+	'title' => __('Song info', 'band-tools'),
 	'fields' => array(
 		array(
 			'key' => 'field_6036d7b066e19',
-			'label' => 'Band',
+			'label' => __('Band', 'band-tools'),
 			'name' => 'band',
 			'type' => 'post_object',
 			'instructions' => '',
@@ -196,7 +196,7 @@ acf_add_local_field_group(array(
 		),
 		array(
 			'key' => 'field_6036d3caa9666',
-			'label' => 'Tracks',
+			'label' => __('Albums', 'band-tools'),
 			'name' => 'tracks',
 			'type' => 'relationship',
 			'instructions' => '',
@@ -214,7 +214,37 @@ acf_add_local_field_group(array(
 			'filters' => array(
 				0 => 'search',
 			),
-			'elements' => '',
+			'elements' => array(
+				0 => 'featured_image',
+			),
+			'min' => '',
+			'max' => '',
+			'return_format' => 'object',
+		),
+		array(
+			'key' => 'field_603addac395f3',
+			'label' => __('Videos', 'band-tools'),
+			'name' => 'video_posts',
+			'type' => 'relationship',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'post_type' => array(
+				0 => 'videos',
+			),
+			'taxonomy' => '',
+			'filters' => array(
+				0 => 'search',
+				1 => 'taxonomy',
+			),
+			'elements' => array(
+				0 => 'featured_image',
+			),
 			'min' => '',
 			'max' => '',
 			'return_format' => 'object',
@@ -239,86 +269,138 @@ acf_add_local_field_group(array(
 	'description' => '',
 ));
 
-// acf_add_local_field_group(array(
-// 	'key' => 'group_603855c7af062',
-// 	'title' => 'WooCommerce',
-// 	'fields' => array(
-// 		array(
-// 			'key' => 'field_6038543ae14e5',
-// 			'label' => 'Product',
-// 			'name' => 'wc_product',
-// 			'type' => 'post_object',
-// 			'instructions' => '',
-// 			'required' => 0,
-// 			'conditional_logic' => array(
-// 				array(
-// 					array(
-// 						'field' => 'field_603856ffbc3ce',
-// 						'operator' => '==empty',
-// 					),
-// 				),
-// 			),
-// 			'wrapper' => array(
-// 				'width' => '',
-// 				'class' => '',
-// 				'id' => '',
-// 			),
-// 			'post_type' => array(
-// 				0 => 'product',
-// 			),
-// 			'taxonomy' => '',
-// 			'allow_null' => 1,
-// 			'multiple' => 0,
-// 			'return_format' => 'object',
-// 			'ui' => 1,
-// 		),
-// 		array(
-// 			'key' => 'field_603856ffbc3ce',
-// 			'label' => 'Categories',
-// 			'name' => 'wc_categories',
-// 			'type' => 'taxonomy',
-// 			'instructions' => '',
-// 			'required' => 0,
-// 			'conditional_logic' => array(
-// 				array(
-// 					array(
-// 						'field' => 'field_6038543ae14e5',
-// 						'operator' => '==empty',
-// 					),
-// 				),
-// 			),
-// 			'wrapper' => array(
-// 				'width' => '',
-// 				'class' => '',
-// 				'id' => '',
-// 			),
-// 			'taxonomy' => 'product_cat',
-// 			'field_type' => 'checkbox',
-// 			'add_term' => 1,
-// 			'save_terms' => 0,
-// 			'load_terms' => 0,
-// 			'return_format' => 'object',
-// 			'multiple' => 0,
-// 			'allow_null' => 0,
-// 		),
-// 	),
-// 	'location' => array(
-// 		array(
-// 			array(
-// 				'param' => 'post_type',
-// 				'operator' => '==',
-// 				'value' => 'albums',
-// 			),
-// 		),
-// 	),
-// 	'menu_order' => 1,
-// 	'position' => 'normal',
-// 	'style' => 'default',
-// 	'label_placement' => 'left',
-// 	'instruction_placement' => 'label',
-// 	'hide_on_screen' => '',
-// 	'active' => true,
-// 	'description' => '',
-// ));
+acf_add_local_field_group(array(
+	'key' => 'group_603adf0d77972',
+	'title' => __('Video info', 'band-tools'),
+	'fields' => array(
+		array(
+			'key' => 'field_603adf4871553',
+			'label' => __('Related posts', 'band-tools'),
+			'name' => 'video_posts',
+			'type' => 'relationship',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => 0,
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'post_type' => array(
+				0 => 'songs',
+				1 => 'albums',
+				2 => 'product',
+			),
+			'taxonomy' => '',
+			'filters' => array(
+				0 => 'search',
+				1 => 'post_type',
+			),
+			'elements' => '',
+			'min' => '',
+			'max' => '',
+			'return_format' => 'object',
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'videos',
+			),
+		),
+	),
+	'menu_order' => 0,
+	'position' => 'normal',
+	'style' => 'default',
+	'label_placement' => 'left',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
+
+acf_add_local_field_group(array(
+	'key' => 'group_603855c7af062',
+	'title' => __('WooCommerce', 'band-tools'),
+	'fields' => array(
+		array(
+			'key' => 'field_6038543ae14e5',
+			'label' => __('Product', 'band-tools'),
+			'name' => 'wc_product',
+			'type' => 'post_object',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => array(
+				array(
+					array(
+						'field' => 'field_603856ffbc3ce',
+						'operator' => '==empty',
+					),
+				),
+			),
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'post_type' => array(
+				0 => 'product',
+			),
+			'taxonomy' => '',
+			'allow_null' => 1,
+			'multiple' => 0,
+			'return_format' => 'object',
+			'ui' => 1,
+		),
+		array(
+			'key' => 'field_603856ffbc3ce',
+			'label' => __('Categories', 'band-tools'),
+			'name' => 'wc_categories',
+			'type' => 'taxonomy',
+			'instructions' => '',
+			'required' => 0,
+			'conditional_logic' => array(
+				array(
+					array(
+						'field' => 'field_6038543ae14e5',
+						'operator' => '==empty',
+					),
+				),
+			),
+			'wrapper' => array(
+				'width' => '',
+				'class' => '',
+				'id' => '',
+			),
+			'taxonomy' => 'product_cat',
+			'field_type' => 'checkbox',
+			'add_term' => 1,
+			'save_terms' => 0,
+			'load_terms' => 0,
+			'return_format' => 'object',
+			'multiple' => 0,
+			'allow_null' => 0,
+		),
+	),
+	'location' => array(
+		array(
+			array(
+				'param' => 'post_type',
+				'operator' => '==',
+				'value' => 'albums',
+			),
+		),
+	),
+	'menu_order' => 1,
+	'position' => 'side',
+	'style' => 'default',
+	'label_placement' => 'top',
+	'instruction_placement' => 'label',
+	'hide_on_screen' => '',
+	'active' => true,
+	'description' => '',
+));
 
 endif;
