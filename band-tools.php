9e1325264917f5accb300cf562e34490ee262924
Olivier van Helden <olivier@van-helden.net>
2021-03-01 18:55:40 -0400

    partial content filter

--- band-tools.php
+++ band-tools.php
@@ -34,6 +34,7 @@ if(is_admin()) {
 	require_once __DIR__ . '/admin/init.php';
 	// require_once __DIR__ . '/admin/wp-dependencies.php';
 }
+require_once __DIR__ . '/templates.php';
 require_once __DIR__ . '/inc/post-types.php';
 require_once __DIR__ . '/inc/widgets.php';
 
