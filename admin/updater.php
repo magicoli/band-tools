4b8638c2147bc605ceef038c3454dfd871c461c0
Olivier van Helden <olivier@van-helden.net>
2021-03-05 21:41:39 -0400

    * queued js script instead of hardcoded

--- admin/updater.php
+++ admin/updater.php
@@ -2,62 +2,13 @@
 
 add_action( 'admin_head', 'bndtls_alter_license_notice', 99, 0 );
 function bndtls_alter_license_notice() {
-  global $wppus_alter_license_form;
-
-  if ( $wppus_alter_license_form['band-tools'] ) {
-
-    return;
-  }
-
-  ?>
-  <style>
-    /* .button.wppus-license-switch {
-      margin-left: 5px;
-      font-weight: normal;
-    } */
-    /* .hidden {
-      display: none;
-    }
-    #wrap_license_band-tools* {
-      display: none;
-    } */
-  </style>
-  <script type="text/javascript">
-    jQuery(function($) {
-
-      if ($('body').hasClass('wppus-license-form-alter-done-band-tools')) {
-        return;
-      }
-
-      var licenseRow = $( ".plugin-update-tr:has([data-package_slug='band-tools'])" );
-      var installRow = $( "[data-slug='band-tools']");
-      if(! installRow) return;
-
-      $(".wrap-license[data-package_slug='band-tools']").each( function( index, element ) {
-        element = $(element);
-
-        if (element.find('.current-license').html().length) {
-          var buttonText = "<?php echo esc_html_e( 'License key', 'band-tools' ); ?>";
-
-          licenseRow.hide();
-          installRow.find('div.row-actions').append(' <span> | <a class="wppus-license-switch band-tools" href="#">' + buttonText + '</a></span>');
-        } else {
-          licenseRow.show();
-          var RegisterText = "<?php echo sprintf(__('Register on %s to get a license key', 'band-tools'), '<a href=https://magiiic.com/wordpress/plugins/band-tools/>Magiiic.com</a>'); ?>";
-          licenseRow.find('.wrap-license').append( "<p class='getlicense'>" + RegisterText + "</p>" );
-        }
-      });
-
-      $('.wppus-license-switch.band-tools').on('click', function(e) {
-        e.preventDefault();
-        licenseRow.toggle();
-      });
-
-      $('body').addClass('wppus-license-form-alter-done-band-tools');
-    });
-
-  </script>
-  <?php
-
-  $wppus_alter_license_form['band-tools'] = true;
+  global $bndtls_alter_license_form;
+  if ( $bndtls_alter_license_form ) return;
+  // plugin_dir_path( MY_PLUGIN )
+  $handle = BNDTLS_SLUG . '/js/wppus-hide-licence-warnings.js';
+  $js = plugins_url($handle);
+  wp_register_script( $handle, $js, array( 'wp-i18n', 'jquery' ) );
+  wp_set_script_translations( $handle, BNDTLS_TXDOM );
+  wp_enqueue_script( $handle, $js );
+  $bndtls_alter_license_form = true;
 }
---
+++ js/wppus-hide-licence-warnings.js
@@ -0,0 +1,47 @@
+const { __, _x, _n, _nx } = wp.i18n;
+
+jQuery(function($) {
+  var BNDTLS_SLUG = 'band-tools';
+  var BNDTLS_DATA_SLUG = BNDTLS_SLUG; // calculated from plugin name, might be different from slug
+  var BNDTLS_TXDOM = BNDTLS_SLUG; // translation text domain, might be different
+
+  var buttonText = __( 'Show/Hide License key', BNDTLS_TXDOM );
+
+  // __( '__', BNDTLS_TXDOM );
+  // _x( '_x', '_x_context', BNDTLS_TXDOM );
+  // _n( '_n_single', '_n_plural', number, BNDTLS_TXDOM );
+  // _nx( '_nx_single', '_nx_plural', number, '_nx_context', BNDTLS_TXDOM );
+
+  var licenseRow = $( ".plugin-update-tr:has([data-package_slug='" + BNDTLS_SLUG + "'])" );
+  var installRow = $( "[data-slug='" + BNDTLS_DATA_SLUG + "']");
+  // installRow.hide();
+
+  if ($('body').hasClass('wppus-license-form-alter-done-' + BNDTLS_SLUG )) {
+    return;
+  }
+
+  if(! installRow) return;
+
+  $(".wrap-license[data-package_slug='" + BNDTLS_SLUG + "']").each( function( index, element ) {
+    element = $(element);
+
+    if (element.find('.current-license').html().length) {
+      // var buttonText = 'License key';
+
+      licenseRow.hide();
+      // installRow.hide();
+      installRow.find('div.row-actions').append('<span> | <a class="wppus-license-switch ' + BNDTLS_SLUG + '" href="#">' + buttonText + '</a></span>');
+      // licenseRow.find('.wrap-license').append(' <span> | <a class="wppus-license-switch ' + BNDTLS_SLUG + '" href="#">' + buttonText + '</a></span>' + " debug " + "[data-slug='" + BNDTLS_DATA_SLUG + "']");
+    } else {
+      licenseRow.show();
+      licenseRow.find('.wrap-license').append( "<p class='getlicense'><?=bndtls_REGISTER_TEXT?></p>" );
+    }
+  });
+
+  $('.wppus-license-switch.' + BNDTLS_SLUG).on('click', function(e) {
+    e.preventDefault();
+    licenseRow.toggle();
+  });
+
+  $('body').addClass('wppus-license-form-alter-done-' + BNDTLS_SLUG);
+});
