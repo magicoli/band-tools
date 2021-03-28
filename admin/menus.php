60ad3d374b4df2f19b902b9eb6431d00454fd1f5
Olivier van Helden <olivier@van-helden.net>
2021-03-28 18:40:24 -0400

    dev workaround (fix admin menu bug when mb-core is active)

--- admin/menus.php
+++ admin/menus.php
@@ -7,13 +7,13 @@ function bndtls_register_settings_pages() {
   add_menu_page(
     'Band Tools', // page title
     'Band Tools', // menu title
-    'list_users', // capability
+    'manage_options', // capability
     'band-tools', // slug
     '', // callable function
   //   // plugin_dir_path(__FILE__) . 'options.php', // slug
   //   // null,	// callable function
-  //   plugin_dir_url(__DIR__) . 'assets/svg-microphone-stand-20x20.svg', // plugin_dir_url(__FILE__) . '../assets/icon-24x24.jpg', // icon url
-  //   2 // position
+    plugin_dir_url(__DIR__) . 'assets/svg-microphone-stand-20x20.svg', // plugin_dir_url(__FILE__) . '../assets/icon-24x24.jpg', // icon url
+    2 // position
   );
 }
 }
@@ -23,17 +23,17 @@ if ( ! is_plugin_active('mb-core/mb-core.php' ) || ! bndtls_get_option( 'develop
 add_filter( 'mb_settings_pages', 'bndtls_dashboard' );
 
 function bndtls_dashboard( $settings_pages ) {
-	$settings_pages[] = [
-      'id' => 'band-tools',
-        'menu_title'  => __( 'Band Tools', 'band-tools' ),
-        'title' =>  __( 'Dashboard', 'band-tools' ),
-        'option_name' => 'bndtls-settings',
-        'position'    => 2,
-        'capability'  => 'manage_options',
-        'class'       => 'no-submit',
-        'icon_url'    => plugin_dir_url(__DIR__) . 'assets/svg-microphone-stand-20x20.svg',
-
-    ];
+  $settings_pages[] = [
+    'id' => 'band-tools',
+    'title' => 'Dashboard',
+    'menu_title'  => __( 'Band Tools', 'band-tools' ),
+    'title' =>  __( 'Band Tools', 'band-tools' ),
+    'option_name' => 'bndtls-settings',
+    'position'    => 2,
+    'capability'  => 'manage_options',
+    'class'       => 'no-submit',
+    'icon_url'    => plugin_dir_url(__DIR__) . 'assets/svg-microphone-stand-20x20.svg',
+  ];
 
 	return $settings_pages;
 }
