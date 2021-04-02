8a613ae98eba4cf94d2cfd84389f247afc050525
Olivier van Helden <olivier@van-helden.net>
2021-04-02 13:26:38 -0400

    v0.7.12.2 * fix numbered list stype bis

--- band-tools.php
+++ band-tools.php
@@ -7,7 +7,7 @@
  * Author URI:      https://magiiic.com/
  * Text Domain:     band-tools
  * Domain Path:     /languages
- * Version:         0.7.12
+ * Version:         0.7.12.2
  *
  * @package         Band_Tools
  *
