87e3ee8c88e4b851ac22349638c1398d51c1d3eb
Olivier van Helden <olivier@van-helden.net>
2021-04-03 22:53:19 -0400

    just a memo

--- templates/content-single-albums.php
+++ templates/content-single-albums.php
@@ -9,14 +9,10 @@
  * @since Twenty Nineteen 1.0
  */
 $content = get_the_content( $more_link_text, $strip_teaser );
-// $albums = MB_Relationships_API::get_connected( [
-//     'id'   => 'rel-band-album',
-//     'from' => get_the_ID(),
-// ] );
-// $songs = MB_Relationships_API::get_connected( [
-//     'id'   => 'rel-band-song',
-//     'from' => get_the_ID(),
-// ] );
+
+// $content_after .= (bndtls_get_option('layout_page_content:songs'))
+// ? bndtls_get_relations(get_post(), [ 'songs' ], [ 'mode' => 'ol' ] ) : '';
+
 ?>
 <div class='<?=$post_type_slug?>-content'>
   <?=$content?>
